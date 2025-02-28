0f6b5cbb2c8e3d275c580a5237a34a048f099c98
AmiratYannis <46192264+AmiratYannis@users.noreply.github.com>
2025-02-28 15:54:09 +0100

    Improve performance due to redirection

--- src/component/Header.js
+++ src/component/Header.js
@@ -59,7 +59,7 @@ const Header = ({ sendPokemonsToParent, IsHomePage }) => {
     return (
         <header className="header">
             <div className="header__logo">
-                <Link to="/" onClick={() => { window.location.href = '/' }}>
+                <Link to="/">
                     <img className="js lg:tw-mx-auto lazyautosizes lazyloaded" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/800px-International_Pok%C3%A9mon_logo.svg.png" />
                 </Link>
             </div>
